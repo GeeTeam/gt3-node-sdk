466de6f20fc3b43a4309979deff740985181f5bf
lhtin <lehuading@qq.com>
2017-03-30 12:39:35 +0800

    add test mode

--- demo/app.js
+++ demo/app.js
@@ -197,6 +197,69 @@ app.post("/gt/validate-slide", function (req, res) {
     });
 });
 
+var test = require('./test');
+app.get("/gt/register-test", function (req, res) {
+
+    // 向极验申请每次验证所需的challenge
+    test.register(function (err, data) {
+        if (err) {
+            console.error(err);
+            return;
+        }
+
+        if (!data.success) {
+            // 进入 failback，如果一直进入此模式，请检查服务器到极验服务器是否可访问
+            // 可以通过修改 hosts 把极验服务器 api.geetest.com 指到不可访问的地址
+
+            // 为以防万一，你可以选择以下两种方式之一：
+
+            // 1. 继续使用极验提供的failback备用方案
+            res.send(data);
+
+            // 2. 使用自己提供的备用方案
+            // todo
+
+        } else {
+            // 正常模式
+            res.send(data);
+        }
+    });
+});
+app.post("/gt/validate-test", function (req, res) {
+
+    // 对ajax提供的验证凭证进行二次验证
+    test.validate({
+        geetest_challenge: req.body.geetest_challenge,
+        geetest_validate: req.body.geetest_validate,
+        geetest_seccode: req.body.geetest_seccode
+
+    }, function (err, success) {
+
+        if (err) {
+
+            // 网络错误
+            res.send({
+                status: "error",
+                info: err
+            });
+
+        } else if (!success) {
+
+            // 二次验证失败
+            res.send({
+                status: "fail",
+                info: '登录失败'
+            });
+        } else {
+
+            res.send({
+                status: "success",
+                info: '登录成功'
+            });
+        }
+    });
+});
+
 var port = 9977;
 app.listen(port, function () {
     console.log('listening at http://localhost:' + port)
---
+++ demo/test.js
@@ -0,0 +1,10 @@
+var Geetest = require('../');
+
+var captcha = new Geetest({
+    geetest_id: '4faf9dd37eb683586977947ce87a866a',
+    geetest_key: 'b4e0149ad455e5434c210ab85d0e22be'
+    // geetest_id: 'e6721cd02ebe3ee262c9b84337367787',
+    // geetest_key: 'a2a4a900f75241998c442274f3e1e492'
+});
+
+module.exports = captcha;
