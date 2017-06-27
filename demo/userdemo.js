ad6768066fd05f91ab33b0ba4a3c053bfccf0db0
melvinren <melvin_ren@icloud.com>
2017-06-27 11:12:27 +0800

    规范id使用

---
+++ demo/userdemo.js
@@ -0,0 +1,8 @@
+var Geetest = require('../gt-sdk');
+
+var captcha = new Geetest({
+    geetest_id: 'cb25522b383a44c171e2e6566654c18f',
+    geetest_key: '2dd2eaa6e00c48d076d863a104da0c13'
+});
+
+module.exports = captcha;
