74cc34fc4cc64cec6bb9a4c6105a9711d902f3af
melvinren <melvin_ren@icloud.com>
2018-05-11 16:05:12 +0800

    update

--- demo/app.js
+++ demo/app.js
@@ -595,6 +595,102 @@ app.post("/gt/validate-phrase", function (req, res) {
     });
 });
 
+var icon = require('./icon');
+app.get("/gt/register-icon", function (req, res) {
+
+    // 向极验申请每次验证所需的challenge
+    icon.register(function (err, data) {
+        if (err) {
+            console.error(err);
+            return;
+        }
+
+        if (!data.success) {
+            // 进入 failback，如果一直进入此模式，请检查服务器到极验服务器是否可访问
+            // 可以通过修改 hosts 把极验服务器 api.geetest.com 指到不可访问的地址
+
+            // 为以防万一，你可以选择以下两种方式之一：
+
+            // 1. 继续使用极验提供的failback备用方案
+            res.send(data);
+
+            // 2. 使用自己提供的备用方案
+            // todo
+
+        } else {
+            // 正常模式
+            res.send(data);
+        }
+    });
+});
+app.post("/gt/validate-icon", function (req, res) {
+
+    // 对ajax提供的验证凭证进行二次验证
+    icon.validate({
+        geetest_challenge: req.body.geetest_challenge,
+        geetest_validate: req.body.geetest_validate,
+        geetest_seccode: req.body.geetest_seccode
+
+    }, function (err, success) {
+
+        if (err) {
+
+            // 网络错误
+            res.send({
+                status: "error",
+                info: err
+            });
+
+        } else if (!success) {
+
+            // 二次验证失败
+            res.send({
+                status: "fail",
+                info: '登录失败'
+            });
+        } else {
+
+            res.send({
+                status: "success",
+                info: '登录成功'
+            });
+        }
+    });
+});
+
+app.post("/gt/verify", function (req, res) {
+    var sense = require('./sense')
+    // 对ajax提供的验证凭证进行二次验证
+    sense.verify({
+        id: req.body.id,
+        challenge: req.body.challenge,
+        idType: req.body.idType,
+        idValue: req.body.username
+    }, function (err, success) {        
+        if (err) {
+
+            // 网络错误
+            res.send({
+                status: "error",
+                info: err
+            });
+
+        } else if (!success) {
+
+            // 二次验证失败
+            res.send({
+                status: "fail",
+                info: '登录失败'
+            });
+        } else {
+            console.log(success)
+            res.send({
+                status: "success",
+                info: '登录成功'
+            });
+        }
+    });
+});
 
 var port = 9977;
 app.listen(port, function () {
---
+++ demo/icon.js
@@ -0,0 +1,7 @@
+var Geetest = require('../gt-sdk');
+
+var captcha = new Geetest({
+    geetest_id: '3101554dd48afd3d07e93bd872d4492c',
+    geetest_key: '969a6fdff7118e8166ad75bafc43ac6d'
+});
+module.exports = captcha;
