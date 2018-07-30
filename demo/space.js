d1a76e63e7ba894f173a10e5c8aef95ac46b9cc0
jyKid <1171639020@qq.com>
2018-07-30 10:59:50 +0800

    add space testpage

---
+++ demo/space.js
@@ -0,0 +1,7 @@
+var Geetest = require('../gt-sdk');
+
+var captcha = new Geetest({
+    geetest_id: 'b09d299e1d3aea1fd52c79668b23f567',
+    geetest_key: 'c888053d112ea75f60c695c05f280daa'
+});
+module.exports = captcha;
