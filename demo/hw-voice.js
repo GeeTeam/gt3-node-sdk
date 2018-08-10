34e85482c40e65a7831bed439b4dfdc086592348
guoaihua <guoaihua@geetest.com>
2018-08-10 17:32:10 +0800

    增加华为app访问接口

---
+++ demo/hw-voice.js
@@ -0,0 +1,6 @@
+var Geetest = require('../gt-sdk');
+var captcha = new Geetest({
+	geetest_id: '98a460967a492ad8f4b2f8ec16b50e56',
+	geetest_key: '435620c3bfb7cad720818a556366d115'
+});
+module.exports = captcha;
